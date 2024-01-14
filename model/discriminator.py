2cedd5f5006c4922e69e74ccff3f1660fe6a694c
Alessandro De Marco <s317626@studenti.polito.it>
2024-01-14 17:16:21 +0100

    First attempt to implement discriminator for GAN

---
+++ model/discriminator.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+from torchsummary import summary
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1, stride=2, padding=1, kernel_size=4, scale_factor=32):
+        super(Discriminator, self).__init__()
+            # Define the convolutional layers
+        self.conv_layers = nn.Sequential(
+            # First convolutional layer
+            nn.Conv2d(in_channels=in_channels, out_channels=64, kernel_size=kernel_size, stride=stride, padding=padding),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            # Second convolutional layer
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=kernel_size, stride=stride, padding=padding),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            # Third convolutional layer
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=kernel_size, stride=stride, padding=padding),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            # Fourth convolutional layer
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=kernel_size, stride=stride, padding=padding),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            # Fifth convolutional layer (output layer)
+            nn.Conv2d(in_channels=512, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+        )
+
+        # Upsampling layer to resize the output to the input size
+        self.upsample = nn.Upsample(scale_factor=scale_factor, mode='bilinear', align_corners=False)
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = self.upsample(x)
+        return x
+    
+model = Discriminator()
+summary(model, input_size=(3, 128, 64))
