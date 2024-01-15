e6db6795428c8853c692f9eb68a4698d9826039f
nicholas.berardo <berardonicholas5@gmail.com>
2024-01-15 11:46:03 +0100

    Cityscapes Dataset

--- cityscapes.py
+++ cityscapes.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
+import torch
 from torch.utils.data import Dataset
-# TODO
+import pandas as pd
+from PIL import Image
+import pathlib
+import os
 
+def pil_loader(path):
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
 
 class CityScapes(Dataset):
     def __init__(self, mode):
         super(CityScapes, self).__init__()
-        # TODO
+        if mode != "train" and mode != "val":
+            return -1
+        self.mode = mode
+        self.data_path = "Cityscapes/images/"+self.mode
+        self.label_path = "Cityscapes/gtFine/"+self.mode
+        self.data = list(pathlib.Path(self.data_path).glob("*/*.png"))
+        self.label = list(pathlib.Path(self.label_path).glob("*/*.png"))
 
     def __getitem__(self, idx):
-        # TODO
+        element = self.data[idx]
+        tmp = element.name.split("_") # 0 = city, 1 and 2 are the numbers, 3 is leftImg8bit.png
+
+        gtColor = self.label_path+"/"+tmp[0]+"/"+ tmp[0]+"_"+tmp[1]+"_"+tmp[2]+"_gtFine_color.png"
+        gtColor_Path = pathlib.Path(gtColor)
+
+        gtLabelTrain = self.label_path+"/"+tmp[0]+"/"+ tmp[0]+"_"+tmp[1]+"_"+tmp[2]+"_gtFine_labelTrainIds.png"
+        gtLabelTrain_Path = pathlib.Path(gtLabelTrain)
+        return pil_loader(element), pil_loader(gtColor_Path), pil_loader(gtLabelTrain_Path)
 
     def __len__(self):
-        # TODO
+        return len(self.data)
